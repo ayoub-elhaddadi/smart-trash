3013fd69ab25f8d8693d96650fd69e24ee42885c
Eldioub <ayoub.elhaddadi.2000@gmail.com>
2023-10-03 22:16:52 +0200

    Added the firebase

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,20 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { initializeApp,provideFirebaseApp } from '@angular/fire/app';
+import { environment } from '../environments/environment';
+import { provideAuth,getAuth } from '@angular/fire/auth';
+import { provideDatabase,getDatabase } from '@angular/fire/database';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    provideFirebaseApp(() => initializeApp(environment.firebase)),
+    provideAuth(() => getAuth()),
+    provideDatabase(() => getDatabase())
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/environments/environment.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  firebase: {
+    projectId: 'new-smart-trash',
+    appId: '1:565256209457:web:f3860486a77bfddf698110',
+    databaseURL: 'https://new-smart-trash-default-rtdb.europe-west1.firebasedatabase.app',
+    storageBucket: 'new-smart-trash.appspot.com',
+    apiKey: 'AIzaSyCZw9n7yv4lNeS5RzaHT_jSIKGeLOMWO_0',
+    authDomain: 'new-smart-trash.firebaseapp.com',
+    messagingSenderId: '565256209457',
+  },
+  production: false,
+};
